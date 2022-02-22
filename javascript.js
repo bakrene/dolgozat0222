7dc1f0b631c37e08657ff98532aa0cc41d3c9f41
Bak René <bakrene1@gmail.com>
2022-02-22 13:21:13 +0100

    java 2.0

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
-var radiogomb;
+var gomb;
 var select;
+var barca;
+var boris;
 
 function elso()
 {
-    var elso = document.getElementsByName("radiogomb");
+    var elso = document.getElementsByName("gomb");
     for(let i = 0; i < elso.length; i++)
     {
         if(elso[i].checked){
             console.log(elso[i].value);
-            radiogomb = elso[i].value;
+            gomb = elso[i].value;
         }
     }
 }
@@ -18,4 +20,17 @@ function masodik()
     var masodik = document.getElementById("masodik");
     select = masodik.options[masodik.selectedIndex].value;
     console.log(masodik.options[masodik.selectedIndex].value);
+}
+
+function negyedik()
+{
+    var negyedik = document.getElementById("negyedik");
+    barca = negyedik;
+    console.log(barca);
+}
+
+function otodik(){
+    var otodik = documnet.getElementById("otodik");
+    boris = otodik;
+    console.log(boris);
 }
